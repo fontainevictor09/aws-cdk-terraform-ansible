852f450991648414cbfffaf7d8de1afe213b6a59
vitor <mb.sahin0@gmail.com>
2025-02-04 10:27:55 +0100

    Terraform s3 var

--- terraform/project/providers.tf
+++ terraform/project/providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-culturedevops"
+    bucket         = ""
     key            = "terraform.tfstate"
     region         = "eu-west-3"
     encrypt        = true
--- terraform/project/variables.tf
+++ terraform/project/variables.tf
@@ -15,3 +15,8 @@ variable "ssh_key_name" {
   description = "Clé SSH créée dans AWS IAM"
   type        = string
 }
+
+variable "s3_bucket_name" {
+  description = "Nom du bucket S3 utilisé pour stocker le backend Terraform"
+  type        = string
+}
